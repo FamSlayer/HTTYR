02b71862fd9debb04251d14fb33f6563756ee8a7
Fuller Taylor <fuller.rules@gmail.com>
2017-09-11 15:21:16 -0400

    Meaningful changes

---
+++ Assets/Code/singleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+ 
+public class singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T _instance;
+ 
+	public static T global
+	{
+		get
+		{
+
+            if (_instance == null) {
+				_instance = (T) FindObjectOfType(typeof(T));
+            }
+
+            return _instance;
+		}
+	}
+ 
+	private static bool applicationIsQuitting = false;
+
+	public void OnDestroy () {
+		applicationIsQuitting = true;
+	}
+}
