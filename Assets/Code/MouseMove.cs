5a544bd5a07eb64a9c99feacc803d711b6f056f6
swood456 <crazedsportsfan@gmail.com>
2017-09-13 14:55:41 -0400

    set up the maze and made the mouse move around randomly. Still need to make the turns work in a better way and encorperate the buzzer

--- Assets/Code/MouseMove.cs
+++ Assets/Code/MouseMove.cs
@@ -22,34 +22,12 @@ public class MouseMove : MonoBehaviour {
     // Update is called once per frame
     void Update ()
     {
-        if(rb.velocity.magnitude < max_speed)
+        // draw a head
+        Debug.DrawLine(transform.position, transform.position + transform.right * 35f, Color.magenta);
+        if (rb.velocity.magnitude < max_speed)
         {
             rb.AddForce(direction * acceleration, ForceMode2D.Force);
         }
-
-        if(PathInDir(transform.up))
-        {
-            //print("boom chika bow wow");
-        }
-	}
-
-
-    bool PathInDir(Vector3 dir)
-    {
-        float distance = 20f;
-        Debug.DrawLine(transform.position, transform.position + dir * distance, Color.magenta);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, distance);//, mask);
-        if( hit.collider != null )
-        {
-            //print("We hit something! " + hit.collider.gameObject.name);
-            return false;
-        }
-        else
-        {
-            //print("We didn't hit anything!");
-            return true;
-        }
-        
     }
 
 
@@ -65,6 +43,59 @@ public class MouseMove : MonoBehaviour {
         else
         {
             print("It was not a dialogue trigger :(");
+        }
+
+        // make the mouse choose a new path semi-randomly
+        // raycast forward, left, and right to find the 3 directions he can go
+        List<RaycastHit2D> collider_hits = new List<RaycastHit2D>();
+        
+        // check in front of mouse, and to left and right of mouse
+        Vector2[] raycast_directions = { transform.up , -1.0f * transform.up , -1.0f * transform.right };
+        RaycastHit2D hit;
+        float trigger_size = collision.GetComponent<BoxCollider2D>().size.y;
+        foreach (Vector2 raycast_dir in raycast_directions)
+        {
+            // raycast from side of trigger box outwards
+            hit = Physics2D.Raycast(collision.transform.position + (Vector3)raycast_dir * trigger_size, raycast_dir);
+            if (hit.collider.tag == "MazeTrigger")
+            {
+                collider_hits.Add(hit);
+            }
+        }
+
+        if(collider_hits.Count == 0)
+        {
+            // have to turn around
+            rb.velocity = new Vector2(0, 0);
+            transform.Rotate(Vector3.forward * 180);
+            direction *= -1.0f;
+            return;
+        }
+        
+        Random rnd = new Random();
+        int index = Random.Range(0, collider_hits.Count);
+        
+
+        Vector2 next_direction = (collider_hits[index].transform.position - collision.transform.position).normalized;
+        if (direction != next_direction)
+        {
+            // we are going to need to turn
+            rb.velocity = new Vector2(0, 0);
+            
+            // hmmm
+            bool is_left_turn = Vector2.Dot(transform.up, next_direction) > 0;
+            if (is_left_turn)
+                transform.Rotate(Vector3.forward * -90);
+            else
+                transform.Rotate(Vector3.forward * 90);
+            //transform.Rotate(Vector3.forward * Vector3.Angle(direction, next_direction));
+
+            direction = (collider_hits[index].transform.position - collision.transform.position);
+            direction.Normalize();
+
+            // this is bad
+            transform.position = collision.transform.position;
+
         }
 
     }
