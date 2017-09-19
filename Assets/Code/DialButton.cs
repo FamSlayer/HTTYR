9965c35fe8fb93ecc22b658fa3c26f8661ba6842
Fuller Taylor <fuller.rules@gmail.com>
2017-09-19 18:46:32 -0400

    uhhh literally "whatever garbage I am on" i guess

---
+++ Assets/Code/DialButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DialButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public DialTurner dt;
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dt.Activate();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        dt.DeActivate();
+    }
+}
---
+++ Assets/Code/DialTurner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialTurner : MonoBehaviour
+{
+    public float turn_speed;
+    bool active = false;
+    float min_angle = 45f;
+    float max_angle = 225f;
+    float angle;
+
+
+    void Awake()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, min_angle);
+        angle = min_angle;
+    }
+    
+    void Update ()
+    {
+        if(active)
+        {
+            print("I am a very active button");
+            //Vector3 mousepos = cam.WorldToViewportPoint(Input.mousePosition);
+            //mousepos = Input.mousePosition;
+            //mousepos = cam.WorldToScreenPoint(Input.mousePosition);
+            //print(mousepos);
+            //Debug.DrawLine(transform.position, mousepos, Color.red);
+            angle += turn_speed * Time.deltaTime;
+            angle = Mathf.Clamp(angle, min_angle, max_angle);
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        }
+		
+	}
+
+    
+    public void Activate()
+    {
+        active = true;
+        print("Activated the button!");
+    }
+
+    public void DeActivate()
+    {
+        active = false;
+        print("DEACTIVATED the button");
+    }
+}
